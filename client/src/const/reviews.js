a6d8b42ed74b940be244ce71c47c12c819a25c2a
Anthony Pecchillo <anthonypecchillo@gmail.com>
2019-10-18 08:21:27 +0700

    Create stubbed list of reviews

---
+++ client/src/const/reviews.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    firstName: 'Angela',
+    lastName: 'Loftus',
+    message: 'Spicy jalapeno bacon ipsum dolor amet capicola meatball shankle, tail andouille bresaola short ribs. Tenderloin kevin drumstick turducken venison burgdoggen spare ribs pork porchetta frankfurter jerky ham hock fatback tongue. Pork belly hamburger chicken alcatra landjaeger, frankfurter meatloaf tongue tenderloin ball tip beef sirloin. Pork belly beef beef ribs brisket drumstick. Tongue pancetta short ribs leberkas shankle t-bone cow jerky chuck frankfurter kielbasa. Tenderloin filet mignon ball tip tri-tip jowl prosciutto swine hamburger burgdoggen salami. Bacon corned beef pork belly short loin bresaola boudin pork chop filet mignon doner.',
+  },
+  {
+    firstName: 'Angela',
+    lastName: 'Loftus',
+    message: 'Spicy jalapeno bacon ipsum dolor amet capicola meatball shankle, tail andouille bresaola short ribs. Tenderloin kevin drumstick turducken venison burgdoggen spare ribs pork porchetta frankfurter jerky ham hock fatback tongue. Pork belly hamburger chicken alcatra landjaeger, frankfurter meatloaf tongue tenderloin ball tip beef sirloin. Pork belly beef beef ribs brisket drumstick. Tongue pancetta short ribs leberkas shankle t-bone cow jerky chuck frankfurter kielbasa. Tenderloin filet mignon ball tip tri-tip jowl prosciutto swine hamburger burgdoggen salami. Bacon corned beef pork belly short loin bresaola boudin pork chop filet mignon doner.',
+  },
+  {
+    firstName: 'Angela',
+    lastName: 'Loftus',
+    message: 'Spicy jalapeno bacon ipsum dolor amet capicola meatball shankle, tail andouille bresaola short ribs. Tenderloin kevin drumstick turducken venison burgdoggen spare ribs pork porchetta frankfurter jerky ham hock fatback tongue. Pork belly hamburger chicken alcatra landjaeger, frankfurter meatloaf tongue tenderloin ball tip beef sirloin. Pork belly beef beef ribs brisket drumstick. Tongue pancetta short ribs leberkas shankle t-bone cow jerky chuck frankfurter kielbasa. Tenderloin filet mignon ball tip tri-tip jowl prosciutto swine hamburger burgdoggen salami. Bacon corned beef pork belly short loin bresaola boudin pork chop filet mignon doner.',
+  },
+  {
+    firstName: 'Angela',
+    lastName: 'Loftus',
+    message: 'Spicy jalapeno bacon ipsum dolor amet capicola meatball shankle, tail andouille bresaola short ribs. Tenderloin kevin drumstick turducken venison burgdoggen spare ribs pork porchetta frankfurter jerky ham hock fatback tongue. Pork belly hamburger chicken alcatra landjaeger, frankfurter meatloaf tongue tenderloin ball tip beef sirloin. Pork belly beef beef ribs brisket drumstick. Tongue pancetta short ribs leberkas shankle t-bone cow jerky chuck frankfurter kielbasa. Tenderloin filet mignon ball tip tri-tip jowl prosciutto swine hamburger burgdoggen salami. Bacon corned beef pork belly short loin bresaola boudin pork chop filet mignon doner.',
+  },
+  {
+    firstName: 'Angela',
+    lastName: 'Loftus',
+    message: 'Spicy jalapeno bacon ipsum dolor amet capicola meatball shankle, tail andouille bresaola short ribs. Tenderloin kevin drumstick turducken venison burgdoggen spare ribs pork porchetta frankfurter jerky ham hock fatback tongue. Pork belly hamburger chicken alcatra landjaeger, frankfurter meatloaf tongue tenderloin ball tip beef sirloin. Pork belly beef beef ribs brisket drumstick. Tongue pancetta short ribs leberkas shankle t-bone cow jerky chuck frankfurter kielbasa. Tenderloin filet mignon ball tip tri-tip jowl prosciutto swine hamburger burgdoggen salami. Bacon corned beef pork belly short loin bresaola boudin pork chop filet mignon doner.',
+  },
+];
