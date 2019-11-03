27bb643275caab72e1df1e6aef0adfe8af5ceda5
Anthony Pecchillo <anthonypecchillo@gmail.com>
2019-11-03 14:25:14 +0700

    Update stubbed review data to more accurately match mockup

--- client/src/const/reviews.js
+++ client/src/const/reviews.js
@@ -2,21 +2,21 @@ export default [
   {
     firstName: 'Angela',
     lastName: 'Loftus',
+    message: 'We had the most amazing wedding invitations created by Seek-A-Design! It was cheaper than the prices I found online, and better quality. I received so many compliments that I’m using them again for an upcoming party I’m throwing. We can’t say enough!',
+  },
+  {
+    firstName: 'Katie',
+    lastName: 'G',
+    message: 'Seek-A-Design is a one stop shop for any of your graphic design needs. The owner brings your vision to life and delivers a finished product better than you ever could have imagined. Seek-A-Design beats the competition in quality, prices and overall experience. There\'s a reason I\'ve returned...5 times!',
+  },
+  {
+    firstName: 'Valentina',
+    lastName: 'C',
     message: 'Spicy jalapeno bacon ipsum dolor amet capicola meatball shankle, tail andouille bresaola short ribs. Tenderloin kevin drumstick turducken venison burgdoggen spare ribs pork porchetta frankfurter jerky ham hock fatback tongue. Pork belly hamburger chicken alcatra landjaeger, frankfurter meatloaf tongue tenderloin ball tip beef sirloin. Pork belly beef beef ribs brisket drumstick. Tongue pancetta short ribs leberkas shankle t-bone cow jerky chuck frankfurter kielbasa. Tenderloin filet mignon ball tip tri-tip jowl prosciutto swine hamburger burgdoggen salami. Bacon corned beef pork belly short loin bresaola boudin pork chop filet mignon doner.',
   },
   {
-    firstName: 'Angela',
-    lastName: 'Loftus',
-    message: 'Spicy jalapeno bacon ipsum dolor amet capicola meatball shankle, tail andouille bresaola short ribs. Tenderloin kevin drumstick turducken venison burgdoggen spare ribs pork porchetta frankfurter jerky ham hock fatback tongue. Pork belly hamburger chicken alcatra landjaeger, frankfurter meatloaf tongue tenderloin ball tip beef sirloin. Pork belly beef beef ribs brisket drumstick. Tongue pancetta short ribs leberkas shankle t-bone cow jerky chuck frankfurter kielbasa. Tenderloin filet mignon ball tip tri-tip jowl prosciutto swine hamburger burgdoggen salami. Bacon corned beef pork belly short loin bresaola boudin pork chop filet mignon doner.',
-  },
-  {
-    firstName: 'Angela',
-    lastName: 'Loftus',
-    message: 'Spicy jalapeno bacon ipsum dolor amet capicola meatball shankle, tail andouille bresaola short ribs. Tenderloin kevin drumstick turducken venison burgdoggen spare ribs pork porchetta frankfurter jerky ham hock fatback tongue. Pork belly hamburger chicken alcatra landjaeger, frankfurter meatloaf tongue tenderloin ball tip beef sirloin. Pork belly beef beef ribs brisket drumstick. Tongue pancetta short ribs leberkas shankle t-bone cow jerky chuck frankfurter kielbasa. Tenderloin filet mignon ball tip tri-tip jowl prosciutto swine hamburger burgdoggen salami. Bacon corned beef pork belly short loin bresaola boudin pork chop filet mignon doner.',
-  },
-  {
-    firstName: 'Angela',
-    lastName: 'Loftus',
+    firstName: 'Tom',
+    lastName: 'F',
     message: 'Spicy jalapeno bacon ipsum dolor amet capicola meatball shankle, tail andouille bresaola short ribs. Tenderloin kevin drumstick turducken venison burgdoggen spare ribs pork porchetta frankfurter jerky ham hock fatback tongue. Pork belly hamburger chicken alcatra landjaeger, frankfurter meatloaf tongue tenderloin ball tip beef sirloin. Pork belly beef beef ribs brisket drumstick. Tongue pancetta short ribs leberkas shankle t-bone cow jerky chuck frankfurter kielbasa. Tenderloin filet mignon ball tip tri-tip jowl prosciutto swine hamburger burgdoggen salami. Bacon corned beef pork belly short loin bresaola boudin pork chop filet mignon doner.',
   },
   {
