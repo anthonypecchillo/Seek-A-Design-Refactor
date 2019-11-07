aa5b1b5e5b5c9a74245415ef4f181454f70dedca
Anthony Pecchillo <anthonypecchillo@gmail.com>
2019-11-07 13:31:42 +0700

    Give SocialIcons a 'big' prop for conditionally rendering big/small versions

--- client/src/components/SocialIcons.jsx
+++ client/src/components/SocialIcons.jsx
@@ -15,40 +15,43 @@ const SocialIconsGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(3, auto);
   grid-column-gap: 10%;
-  alight-items: center;
-  justify-self: right;
+  align-items: start;
+  justify-items: center;
+  justify-self: center;
 
   font-size: 22px;
-  padding-top: 15px;
-  margin-right: 27.5%;
+  margin-right: ${({big}) => big ? '0' : '27.5%'};
+  padding-top: ${({big}) => big ? '0' : '5px'};
+  margin-top: 10px;
+  margin-bottom: 10px;
 `;
 
 const Circle = styled.a`
   background: ${({color}) => color};
-  width: 45px;
-  height: 45px;
-  border-radius: 50%;;
+  width: ${({big}) => big ? '76px' : '45px'};
+  height: ${({big}) => big ? '76px' : '45px'};
+  border-radius: 50%;
   text-align: center;
 `;
 
 const SocialIcon = styled.i`
   color: white;
-  font-size: 26px;
-  padding-top: 10px;
+  font-size: ${({big}) => big ? '48px' : '26px'};
+  padding-top: ${({big}) => big ? '15px' : '10px'};
 `;
 
-const SocialIcons = () => (
-  <SocialIconsGrid>
-    <Circle color="#06837A" href={FACEBOOK_URL} rel="noopener noreferrer" target="_blank">
-      <SocialIcon className="fab fa-facebook-f" />
+const SocialIcons = ({ big }) => (
+  <SocialIconsGrid big={big}>
+    <Circle color="#06837A" href={FACEBOOK_URL} rel="noopener noreferrer" target="_blank" big={big}>
+      <SocialIcon className="fab fa-facebook-f" big={big} />
     </Circle>
-    <Circle color="rgb(87, 11, 95)" href={TWITTER_URL} rel="noopener noreferrer" target="_blank">
-      <SocialIcon className="fab fa-twitter" />
+    <Circle color="rgb(87, 11, 95)" href={TWITTER_URL} rel="noopener noreferrer" target="_blank" big={big}>
+      <SocialIcon className="fab fa-twitter" big={big} />
     </Circle>
-    <Circle color="#06837A" href={INSTAGRAM_URL} rel="noopener noreferrer" target="_blank">
-      <SocialIcon className="fab fa-instagram" />
+    <Circle color="#06837A" href={INSTAGRAM_URL} rel="noopener noreferrer" target="_blank" big={big}>
+      <SocialIcon className="fab fa-instagram" big={big} />
     </Circle>
   </SocialIconsGrid>
-)
+);
 
 export default SocialIcons;
